012780bc998c860bfb171bcfd160ba1055eb961d
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-05-09 15:59:56 +0300

    Add Routing - add import { Routes, Route,BrowserRouter,NavLink, } from "react-router-dom" - npm i react-router-dom

--- redmango/src/Container/App.tsx
+++ redmango/src/Container/App.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Footer, Header } from "../Components/Layout";
 import { Home } from "../Pages";
-
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div>
       <Header />
-      <Home />
+      <div className="pb-5">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </div>
       <Footer />
     </div>
   );
 }
+
 export default App;
