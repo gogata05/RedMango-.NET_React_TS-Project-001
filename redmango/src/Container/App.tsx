2af9668708ee99ce296886488e2a322492123d59
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-05-09 16:02:59 +0300

    add Not Found

--- redmango/src/Container/App.tsx
+++ redmango/src/Container/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Footer, Header } from "../Components/Layout";
-import { Home } from "../Pages";
+import { Home, NotFound } from "../Pages";
 import { Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -9,11 +9,11 @@ function App() {
       <div className="pb-5">
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </div>
       <Footer />
     </div>
   );
 }
-
 export default App;
