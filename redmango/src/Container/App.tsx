a4a92f85fa25277b0a2838c4340d780092828391
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-05-09 04:25:18 +0300

    add Header and Footer Component - S "rfce" - ReactFunctionalExportComponent

--- redmango/src/Container/App.tsx
+++ redmango/src/Container/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React from "react";
+import { Footer, Header } from "../Components/Layout";
 
 function App() {
   return (
-    <div className="text-success">App</div>
+    <div>
+      <Header />
+      Main Component
+      <Footer />
+    </div>
   );
 }
 
