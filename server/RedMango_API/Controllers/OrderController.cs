16b55b5edb624eaeb3b13ed27f3909fad474f4b1
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-05-25 13:58:42 +0300

    Add Search and Status Filter on Order

--- server/RedMango_API/Controllers/OrderController.cs
+++ server/RedMango_API/Controllers/OrderController.cs
@@ -23,21 +23,35 @@ namespace RedMango_API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<ApiResponse>> GetOrders(string? userId)
+        public async Task<ActionResult<ApiResponse>> GetOrders(string? userId,
+            string searchString, string status)
         {
             try
             {
-                var orderHeaders = _db.OrderHeaders.Include(u => u.OrderDetails)
-                    .ThenInclude(u => u.MenuItem)
-                    .OrderByDescending(u => u.OrderHeaderId);
+                IEnumerable<OrderHeader> orderHeaders =
+                    _db.OrderHeaders.Include(u => u.OrderDetails)
+                        .ThenInclude(u => u.MenuItem)
+                        .OrderByDescending(u => u.OrderHeaderId);
+
+
                 if (!string.IsNullOrEmpty(userId))
                 {
                     _response.Result = orderHeaders.Where(u => u.ApplicationUserId == userId);
                 }
-                else
+
+                if (!string.IsNullOrEmpty(searchString))
                 {
-                    _response.Result = orderHeaders;
+                    orderHeaders = orderHeaders
+                        .Where(u => u.PickupPhoneNumber.ToLower().Contains(searchString.ToLower()) ||
+                                    u.PickupEmail.ToLower().Contains(searchString.ToLower())
+                                    || u.PickupName.ToLower().Contains(searchString.ToLower()));
                 }
+
+                if (!string.IsNullOrEmpty(status))
+                {
+                    orderHeaders = orderHeaders.Where(u => u.Status.ToLower() == status.ToLower());
+                }
+
 
                 _response.StatusCode = HttpStatusCode.OK;
                 return Ok(_response);
