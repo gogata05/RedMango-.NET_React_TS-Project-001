30b1492e7048ead8ac11b38ae88d3e42693abb6c
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-05-27 10:10:30 +0300

    Create Payment API

---
+++ client/src/Apis/paymentApi.ts
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const paymentApi = createApi({
+  reducerPath: "paymentApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://localhost:7117/api/",
+  }),
+  endpoints: (builder) => ({
+    initiatePayment: builder.mutation({
+      query: (userId) => ({
+        url: "menuitem",
+        method: "POST",
+        params: {
+          userId: userId,
+        },
+      }),
+    }),
+  }),
+});
+
+export const { useInitiatePaymentMutation } = paymentApi;
+export default paymentApi;
