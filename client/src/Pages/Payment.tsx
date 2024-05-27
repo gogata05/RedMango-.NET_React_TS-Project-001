55f18d0ec2afc7e4b4601812a34486ff27b0ca97
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-05-27 10:51:19 +0300

    add stripe payment UI at /payment

--- client/src/Pages/Payment.tsx
+++ client/src/Pages/Payment.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { useLocation } from "react-router";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { PaymentForm } from "../Components/Page/Payment";
 
 function Payment() {
   const {
     state: { apiResult, userInput },
   } = useLocation();
 
-  console.log(apiResult);
-  console.log(userInput);
-  return <div>Payment</div>;
+  const stripePromise = loadStripe(
+    "pk_test_51PDiPUHnhnqR1L0xHDf9eAwQOR6OdzELRFra7t5mgP1WLPHMl2CV0Rh2q7tYJJrB8R5cqrAyPQJ58lmjny9QGkBX00RCReXyFq"
+  );
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret: apiResult.clientSecret,
+  };
+
+  return (
+    <Elements stripe={stripePromise} options={options}>
+      <PaymentForm />
+    </Elements>
+  );
 }
 
 export default Payment;
