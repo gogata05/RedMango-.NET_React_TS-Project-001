79a186e55d91c24cb633426dfb021943ebdad74d
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-05-28 15:18:31 +0300

    Order Status info on OrderList

--- client/src/Interfaces/orderHeaderModel.ts
+++ client/src/Interfaces/orderHeaderModel.ts
@@ -1,6 +1,7 @@
+import { SD_Status } from "../Utility/SD";
 import orderDetail from "./orderDetailModel";
 
-export default interface orderHeader {
+export default interface orderHeaderModel {
   orderHeaderId?: number;
   pickupName?: string;
   pickupPhoneNumber?: string;
@@ -10,7 +11,7 @@ export default interface orderHeader {
   orderTotal?: number;
   orderDate?: Date;
   stripePaymentIntentID?: string;
-  status?: string;
+  status?: SD_Status;
   totalItems?: number;
   orderDetails?: orderDetail[];
 }
