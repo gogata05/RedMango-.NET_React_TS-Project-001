2fbdfcd13c389f71f865564ba38563001300a132
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-05-29 19:18:43 +0300

    add Banner with input for search at home

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { Banner } from "../Components/Page/Common";
 import { MenuItemList } from "../Components/Page/MenuItems";
 
 function Home() {
   return (
     <div>
+      <Banner />
       <div className="container p-2">
         <MenuItemList />
       </div>
     </div>
   );
 }
-
 export default Home;
